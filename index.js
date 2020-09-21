66b160c14fb5ff9d0c503d0547ed314cfd08c0c8
Kabirdas Henry <kbrdsk@gmail.com>
2020-09-21 09:32:47 -0700

    refactored for better error handling

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const util = require("util");
 const app = express();
 const readFile = util.promisify(fs.readFile);
 
-app.use(check404);
 app.get("/", fileDelivery("./index.html"));
 app.get("/about", fileDelivery("./about.html"));
 app.get("/contact-me", fileDelivery("./contact-me.html"));
+app.use(check404);
+app.use(reportError);
 app.listen(8080);
 
-async function check404(req, res, next) {
-	if (["/", "/about", "/contact-me"].includes(req.url)) next();
-	else res.end(await readFile("./404.html"));
+function check404(req, res) {
+	readFile("./404.html")
+		.then((data) => res.end(data))
+		.catch((err) => reportError(err, req, res));
 }
 
-function fileDelivery(filepath, res) {
-	return async (req, res) => res.end(await readFile(filepath));
+function fileDelivery(filepath) {
+	return (req, res) =>
+		readFile(filepath)
+			.then((data) => res.end(data))
+			.catch((err) => reportError(err, req, res));
+}
+
+function reportError(err, req, res) {
+	res.end("There was an error.");
 }
